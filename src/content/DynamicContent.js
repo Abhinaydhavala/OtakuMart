29eb0e7cbef7c3df9b63cac441c5d56987fcb758
abhinayd96 <106115432+abhinayd96@users.noreply.github.com>
2024-12-17 18:13:51 -0500

    Created Filter Component, created a centralised file for content

--- src/content/DynamicContent.js
+++ src/content/DynamicContent.js
@@ -36,4 +36,84 @@ const About = {
   },
 };
 
-export default About;
+// New filter content
+const Filter = {
+  heading: "Filter",
+  popularAnime: {
+    title: "Popular Anime",
+    options: [
+      "One Piece",
+      "Solo Leveling",
+      "HunterXHunter",
+      "Naruto",
+      "SpyXFamily",
+      "Demon Slayer",
+    ],
+  },
+  priceRange: {
+    title: "Price Range",
+    min: 0,
+    max: 100,
+  },
+  products: {
+    title: "Products",
+    options: [
+      "T-Shirts",
+      "Joggers",
+      "Hoodies",
+      "Sweatshirts",
+      "Shorts",
+      "Fullhand Sleeves T-Shirts",
+    ],
+  },
+  size: {
+    title: "Size",
+    options: ["S", "M", "L", "XL", "XXL"],
+  },
+};
+
+const OtakuMartContent = {
+  heroCarousel: [
+    {
+      image: "./assets/HeroBanner.jpg",
+      title: "Welcome to OtakuMart",
+      description: "Your one-stop shop for anime merchandise!",
+    },
+    {
+      image: "./assets/SpyXFamily.jpg",
+      title: "Spy X Family Collection",
+      description: "Exclusive products for Spy X Family fans!",
+    },
+    {
+      image: "./assets/LuffyCarousel.jpg",
+      title: "OnePiece Arrivals",
+      description: "Discover the latest Luffy Gear 5 collection.",
+    },
+  ],
+  products: {
+    heading: "Our Products",
+    items: Array(6).fill({
+      image: "./assets/tshirt.jpg",
+      title: "Product Title",
+      description: "Some description of the product.",
+    }),
+  },
+  bestSellers: {
+    heading: "Best Sellers",
+    items: [
+      {
+        image: "./assets/SpyXFamily.jpg",
+        title: "Spy X Family",
+      },
+      {
+        image: "./assets/BlueLock.jpeg",
+        title: "Blue Lock",
+      },
+    ],
+  },
+  footer: {
+    text: "© 2024 OtakuMart. All rights reserved.",
+  },
+};
+
+export { About, Filter, OtakuMartContent };
